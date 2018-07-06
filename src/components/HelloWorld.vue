465d99762cbc5d31cc4169b71daff03e59f2afcb
automatedcommunication <aletheia@aletheias-MacBook-Air.local>
2018-07-06 10:44:51 -0700

    added app files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <body>
+    <h1>{{ msg }}</h1>
+  </body>
+</template>
+
+<script>
+var msg = prompt('What is your name?')
+document.write('Oh my gosh, ' + msg + ', we just made a unique web experience together!')
+</script>
+
+<style>
+body {
+background-color: #f0db4f;
+font-family: sans-serif;
+text-align: center;
+color: #323330;
+font-size:  100px;
+}
+h1 {
+margin-top: -150px;
+}
+</style>
